07269b9a3c34e75e2edd6aa957027b5982f230aa
S <2888818l@student.gla.ac.uk>
2025-07-14 15:30:11 +0100

    sucessfully gets data from api

--- f1app/f1StatsApp/urls.py
+++ f1app/f1StatsApp/urls.py
@@ -3,4 +3,6 @@ from f1StatsApp import views
 app_name = 'f1statsapp'
 urlpatterns = [
 path('', views.lap_times, name='lap_times'),
+path('hamilton/', views.hamilton_monaco_race, name='hamilton_data'),
+
 ]
--- f1app/f1StatsApp/views.py
+++ f1app/f1StatsApp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from django.http import JsonResponse
 # Create your views here.
 
 def lap_times(request):
     # Example: Get lap times for Lewis Hamilton in 2022 Bahrain GP
     response = requests.get("https://api.openf1.org/v1/lap_times", params={
         "year": 2022,
-        "session_name": "R",
-        "driver_number": 44  # Hamilton
+        "driver_number": 1  # Hamilton
     })
 
     lap_data = response.json()
@@ -16,3 +16,12 @@ def lap_times(request):
         'lap_times': lap_data[:20]  # limit for display
     }
     return render(request, 'f1statsapp/lap_times.html', context)
+
+
+def hamilton_monaco_race(request):
+
+    response = requests.get("https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158")
+    data = response.json()
+    print(response.url)
+
+    return JsonResponse(data, safe=False)
